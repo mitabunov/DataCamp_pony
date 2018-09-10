63a8437750e2ecab1f3a466e7deec534b7896769
Albert <albert.kuc@gmail.com>
2018-09-10 21:32:13 +0100

    Created new folder for data science toolbox and examples for functions
    Added readme and tweets example

---
+++ 03_data_science_toolbox_1/Functions and Tuples/Tweets_alt.py
@@ -0,0 +1,44 @@
+"""
+Load a dataset into a DataFrame that contains Twitter data.Iterate over entries in a column to build a dictionary in
+which the keys are the names of languages and the values are the number of tweets in the given language.
+
+Same functionality as Tweets.py but iteration written within a function
+"""
+
+# Import pandas
+import pandas as pd
+
+# Import Twitter data as DataFrame: df
+tweets_df = pd.read_csv('tweets.csv')
+
+
+# Define count_entries()
+def count_entries(df, col_name):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: langs_count
+    langs_count = {}
+
+    # Extract column from DataFrame: col
+    col = df[col_name]
+
+    # Iterate over lang column in DataFrame
+    for entry in col:
+
+        # If the language is in langs_count, add 1
+        if entry in langs_count.keys():
+            langs_count[entry] += 1
+        # Else add the language to langs_count, set the value to 1
+        else:
+            langs_count[entry] = 1
+
+    # Return the langs_count dictionary
+    return langs_count
+
+
+# Call count_entries(): result
+result = count_entries(tweets_df, 'lang')
+
+# Print the result
+print(result)
---
+++ 03_data_science_toolbox_1/README.md
@@ -0,0 +1,5 @@
+# Python Data Science Toolbox (Part 1)
+#### Contains chapters:
+1. Functions and tuples
+2. Default arguments, variable-length arguments and scope
+3. Lambda functions and error-handling
