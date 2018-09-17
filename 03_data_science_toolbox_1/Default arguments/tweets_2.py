205c4942d281b08ff884fe6c30f0dd21221d67af
Albert <albert.kuc@gmail.com>
2018-09-17 20:10:07 +0100

    Updated tweets files in Datascience toolbox 1

--- 03_data_science_toolbox_1/Default arguments/tweets_2.py
+++ 03_data_science_toolbox_1/Default arguments/tweets_2.py
@@ -1,14 +1,50 @@
+"""
+Bringing it all together (2):
+Allow the user to pass as many column names as the user would like!
+"""
+
 # Import pandas
 import pandas as pd
 
 # Import Twitter data as DataFrame: df
-df = pd.read_csv(r'../Functions and Tuples/tweets.csv')
+tweets_df = pd.read_csv(r'../Functions and Tuples/tweets.csv')
 
 
-df2 = pd.read_csv(r'C:/Users/Mikele\/Desktop/Datacamp/digits.csv')
+# Define count_entries()
+def count_entries(df, *args):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
 
-df3 = pd.read_csv(r'C:/Users/Mikele\/Desktop/Datacamp/titanic.csv')
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
 
-print(df3.loc[[0], ['Sex']])
-print(df3.loc[[0], ['Age']])
-print(df3.iloc[[0], [5]])
+    # Iterate over column names in args
+    for col_name in args:
+
+        # Extract column from DataFrame: col
+        col = df[col_name]
+
+        # Iterate over the column in DataFrame
+        for entry in col:
+
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
+
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
+
+    # Return the cols_count dictionary
+    return cols_count
+
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
+
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'lang', 'source')
+
+# Print result1 and result2
+print(result1)
+print(result2)
