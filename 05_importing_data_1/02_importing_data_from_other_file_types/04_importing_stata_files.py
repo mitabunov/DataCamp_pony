f8a70ef5b8d31d49db22567c514d4d837b27a939
Albert <albert.kuc@gmail.com>
2018-10-06 23:26:12 +0100

    Added files to chapter 2 folder and datasets
    Added import sas and stata files examples to chapter 2 folder along with
    datasets and slides for chapter 2

---
+++ 05_importing_data_1/02_importing_data_from_other_file_types/04_importing_stata_files.py
@@ -0,0 +1,28 @@
+"""
+Importing Stata files:
+Here, you'll gain expertise in importing Stata files as DataFrames using the pd.read_stata() function from pandas.
+The last exercise's file, 'disarea.dta', is still in your working directory.
+
+INSTRUCTIONS:
+* Use pd.read_stata() to load the file 'disarea.dta' into the DataFrame df.
+* Print the head of the DataFrame df.
+* Visualize your results by plotting a histogram of the column disa10. We’ve already provided this code for you, so just
+  run it!
+"""
+
+
+# Import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata(r'../_datasets/disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
