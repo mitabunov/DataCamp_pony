432586780c634b7fd7b6ceff93c0ba2bfdab1b42
Albert <albert.kuc@gmail.com>
2018-11-21 21:08:37 +0000

    Added header_none to initial read_csv to avoid overwriting first column.
    Generated dropped and clean data were missing first row from initial
    file as it was assigned as header and therefore overwrited when
    renaming columns. In datasets new dropped and clean versions generated.

--- 08_pandas_foundations/04_case_study/02_re-assigning_column_names.py
+++ 08_pandas_foundations/04_case_study/02_re-assigning_column_names.py
@@ -20,7 +20,7 @@ Instructions:
 # Import pandas
 import pandas as pd
 # Read in the data file: df
-df = pd.read_csv('../_datasets/NOAA_QCLCD_2011_hourly_13904.txt')
+df = pd.read_csv('../_datasets/NOAA_QCLCD_2011_hourly_13904.txt', header=None)
 
 column_labels = 'Wban,date,Time,StationType,sky_condition,sky_conditionFlag,visibility,visibilityFlag,wx_and_obst_to_vision,wx_and_obst_to_visionFlag,dry_bulb_faren,dry_bulb_farenFlag,dry_bulb_cel,dry_bulb_celFlag,wet_bulb_faren,wet_bulb_farenFlag,wet_bulb_cel,wet_bulb_celFlag,dew_point_faren,dew_point_farenFlag,dew_point_cel,dew_point_celFlag,relative_humidity,relative_humidityFlag,wind_speed,wind_speedFlag,wind_direction,wind_directionFlag,value_for_wind_character,value_for_wind_characterFlag,station_pressure,station_pressureFlag,pressure_tendency,pressure_tendencyFlag,presschange,presschangeFlag,sea_level_pressure,sea_level_pressureFlag,record_type,hourly_precip,hourly_precipFlag,altimeter,altimeterFlag,junk'
 list_to_drop = ['sky_conditionFlag', 'visibilityFlag', 'wx_and_obst_to_vision', 'wx_and_obst_to_visionFlag',
@@ -42,5 +42,6 @@ df_dropped = df.drop(list_to_drop, axis='columns')
 
 # Print the output of df_dropped.head()
 print(df_dropped.head())
+print(df_dropped['Time'].head())
 
 df_dropped.to_csv('../_datasets/NOAA_QCLCD_2011_dropped.txt', index=False)
