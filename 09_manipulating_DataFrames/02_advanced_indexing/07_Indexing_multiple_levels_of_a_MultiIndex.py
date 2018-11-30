d4834e1cf7bc08f8be0c1319d9895c07b8e39db3
Albert <albert.kuc@gmail.com>
2018-11-30 23:53:24 +0000

    Added examples of Hierarchical Indexing to Manipulating DataFrames ch2
    Added 4 examples of the concept of MultiIndex, creating and sorting
    MultiIndex, access and slice DataFrame.

---
+++ 09_manipulating_DataFrames/02_advanced_indexing/07_Indexing_multiple_levels_of_a_MultiIndex.py
@@ -0,0 +1,41 @@
+"""
+Indexing multiple levels of a MultiIndex:
+Looking up indexed data is fast and efficient. And you have already seen that lookups based on the outermost level of a
+MultiIndex work just like lookups on DataFrames that have a single-level Index.
+
+Looking up data based on inner levels of a MultiIndex can be a bit trickier. In this exercise, you will use your sales
+DataFrame to do some increasingly complex lookups.
+
+The trickiest of all these lookups are when you want to access some inner levels of the index. In this case, you need to
+use slice(None) in the slicing parameter for the outermost dimension(s) instead of the usual :, or use pd.IndexSlice.
+You can refer to the pandas documentation for more details (http://pandas.pydata.org/pandas-docs/stable/advanced.html).
+For example, in the video, Dhavide used the following code to extract rows from all Symbols for the dates Oct. 3rd
+through 4th inclusive:
+    stocks.loc[(slice(None), slice('2016-10-03', '2016-10-04')), :]
+
+Pay particular attention to the tuple
+    (slice(None), slice('2016-10-03', '2016-10-04')).
+
+Instructions:
+*   Look up data for the New York column ('NY') in month 1.
+*   Look up data for the California and Texas columns ('CA', 'TX') in month 2.
+*   Look up data for all states in month 2. Use (slice(None), 2) to extract all rows in month 2.
+"""
+
+
+import pandas as pd
+sales = pd.read_csv('../_datasets/sales.csv')
+month = (1, 2, 1, 2, 1, 2)
+state = ('CA', 'CA', 'NY', 'NY', 'TX', 'TX')
+sales.month = month
+sales['state'] = state
+sales.set_index(['state', 'month'], inplace=True)
+# *******************************************
+
+# Look up data for NY in month 1: NY_month1
+NY_month1 = sales.loc[('NY', 1), :]
+# Look up data for CA and TX in month 2: CA_TX_month2
+CA_TX_month2 = sales.loc[(['CA', 'TX'], 2), :]
+# Look up data for all states in month 2: all_month2
+all_month2 = sales.loc[(slice(None), 2), :]
+print(all_month2)
