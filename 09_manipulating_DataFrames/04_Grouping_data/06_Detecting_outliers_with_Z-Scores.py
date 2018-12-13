4d486d4c4831ca38075ec7ecc1bb2abaa61af8ff
Albert <albert.kuc@gmail.com>
2018-12-13 22:49:25 +0000

    Added Groupby and Transformation to Manipulating DataFrames ch4
    Added example of using .transform() method after grouping to apply a
    function to groups of data indepenently and filter outliers with z-score
    value.
    Added example of using .groupby() and .transform() to fill missing data
    for each group.
    Added example of using .apply() method on groupby object when performs
    an arbitrary function on each of the groups.
    
    Added chapter slides

---
+++ 09_manipulating_DataFrames/04_Grouping_data/06_Detecting_outliers_with_Z-Scores.py
@@ -0,0 +1,41 @@
+"""
+Detecting outliers with Z-Scores:
+As Dhavide demonstrated in the video using the zscore function, you can apply a .transform() method after grouping to
+apply a function to groups of data independently. The z-score is also useful to find outliers: a z-score value of +/- 3
+is generally considered to be an outlier.
+
+In this example, you're going to normalize the Gapminder data in 2010 for life expectancy and fertility by the z-score
+per region. Using boolean indexing, you will filter out countries that have high fertility rates and low life expectancy
+for their region.
+
+The Gapminder DataFrame for 2010 indexed by 'Country' is provided for you as gapminder_2010.
+
+Instructions:
+*   Import zscore from scipy.stats.
+*   Group gapminder_2010 by 'region' and transform the ['life','fertility'] columns by zscore.
+*   Construct a boolean Series of the bitwise or between standardized['life'] < -3 and standardized['fertility'] > 3.
+*   Filter gapminder_2010 using .loc[] and the outliers Boolean Series. Save the result as gm_outliers.
+*   Print gm_outliers. This has been done for you, so hit 'Submit Answer' to see the results.
+"""
+
+import pandas as pd
+
+# Read the CSV file into a DataFrame and sort the index: gapminder
+gapminder = pd.read_csv('../_datasets/gapminder_tidy.csv', index_col=['Country']).sort_index()
+# Filter gapminder by Year==2010 and remove column Year
+gapminder_2010 = gapminder[gapminder['Year']==2010].drop('Year', axis=1)
+print(gapminder_2010.head())
+print(50*'-'+'\n'+50*'-')
+# *********************************************
+
+from scipy.stats import zscore
+
+# Group gapminder_2010: standardized
+standardized = gapminder_2010.groupby('region')['life', 'fertility'].transform(zscore)
+# Construct a Boolean Series to identify outliers: outliers
+outliers = (standardized['life'] < -3) | (standardized['fertility'] > 3)
+# Filter gapminder_2010 by the outliers: gm_outliers
+gm_outliers = gapminder_2010.loc[outliers]
+
+# Print gm_outliers
+print(gm_outliers)
